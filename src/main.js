58ea057442f29de78731431c7b85718fd47ed210
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2018-07-09 23:54:12 +0600

    add Message Component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Message from './Message.vue';
+
+Vue.component('app-message', Message);
 
 new Vue({
   el: '#app',
