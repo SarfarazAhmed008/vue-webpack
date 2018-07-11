766b20876aa088b413ce1eba33473fa3357aa7b8
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2018-07-11 15:36:41 +0600

    view routers added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+      <router-view></router-view>
+      <hr>
       <app-message></app-message>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import Message from './Message.vue';
+import Home from './Home.vue';
+import Users from './Users.vue';
 
 Vue.component('app-message', Message);
 
+Vue.use(VueRouter);
 
+const routes = [
+    { path : '/users', component : Users },
+    { path : '/', component : Home },
+];
+
+const router = new VueRouter({
+    routes
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    router,
+    render: h => h(App)
+});
